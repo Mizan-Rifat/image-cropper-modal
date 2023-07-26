8f1db56295083d460487be8caf43dcf11c523a4a
Mizan-Rifat <mizanrifat001@gmail.com>
2023-07-26 21:29:53 +0600

    published

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'image-cropper-modal',
   plugins: [tsconfigPaths(), react()],
   resolve: {
     alias: {
